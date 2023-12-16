bf2cfa030a3e6670b462baa766fee537050015ff
ArayaFeisa <arayafeisas@gmail.com>
2023-12-16 22:18:49 +0700

    activity_log initial

--- activity_log.php
+++ activity_log.php
@@ -1,36 +1,34 @@
 <?php
 include('connection.php');
 
-// Fungsi untuk menghapus tugas
-if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
-    $taskId = $_GET['id'];
-    $deleteSql = "DELETE FROM deadlinemu.Task WHERE TaskID = $taskId";
+// Fungsi untuk menambahkan log aktivitas berkaitan dengan tugas
+function addTaskActivityLog($userID, $taskID, $logType) {
+    global $connection;
 
-    if (mysqli_query($connection, $deleteSql)) {
-        header("Location: task.php");
-        exit();
+    $timestamp = date('Y-m-d H:i:s');
+    $insertSql = "INSERT INTO deadlinemu.activitylog (UserID, TaskID, Timestamp, LogType) 
+                  VALUES ($userID, $taskID, '$timestamp', '$logType')";
+
+    if (mysqli_query($connection, $insertSql)) {
+        return true;
     } else {
-        die("Error deleting task: " . mysqli_error($connection));
+        die("Error adding activity log: " . mysqli_error($connection));
     }
 }
 
-// Query untuk mengambil data tugas
-$sql = "SELECT * FROM deadlinemu.Task";
-$result = mysqli_query($connection, $sql);
+// Ambil data aktivitas log dari database
+$selectSql = "SELECT * FROM deadlinemu.activitylog WHERE LogType IN ('Create Task', 'Edit Task', 'Delete Task')";
+$result = mysqli_query($connection, $selectSql);
 
-// Memeriksa apakah query berhasil dieksekusi
-if (!$result) {
-    die("Error fetching tasks: " . mysqli_error($connection));
-}
+// Tampilan HTML untuk menampilkan aktivitas log
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DeadlineMU - Category</title>
+    <title>DeadlineMU - Activity Log</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="stylecategory.css">
@@ -41,55 +39,43 @@ if (!$result) {
         <div class="brand">
             DeadlineMU
         </div>
-        <a href="homepage.php" class="active">Home</a>
+        <a href="homepage.php">Home</a>
         <a href="task.php">Task</a>
         <a href="category.php">Category</a>
         <a href="task_and_category.php">Task and Category</a>
         <a href="activity_log.php">Task Log</a>
-        <a href="bookmark.php">Bookmark</a>
+        <a href="#">Bookmark</a>
         <a href="#">Logout</a>
     </div>
 
     <div class="container">
-        <h2 class="mt-5 mb-3">Task</h2>
-        <a href="v_addtask.php" class="btn btn-primary my-3">Add New Task</a>
+        <h2 class="mt-5 mb-3">Activity Log</h2>
         <table class="table">
             <thead>
                 <tr>
                     <th>No</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Due Date</th>
-                    <th>Priority</th>
-                    <th>Status</th>
                     <th>User ID</th>
-                    <th>Category ID</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
+                    <th>Task ID</th>
+                    <th>Timestamp</th>
+                    <th>Log Type</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                // Menampilkan data tugas
                 if (mysqli_num_rows($result) > 0) {
                     $count = 1;
                     while ($row = mysqli_fetch_assoc($result)) {
                         echo "<tr>
                                 <td>{$count}</td>
-                                <td>{$row['Title']}</td>
-                                <td>{$row['Description']}</td>
-                                <td>{$row['DueDate']}</td>
-                                <td>{$row['Priority']}</td>
-                                <td>{$row['Status']}</td>
                                 <td>{$row['UserID']}</td>
-                                <td>{$row['CategoryID']}</td>
-                                <td><a href='v_edittask.php?id={$row['TaskID']}' class='btn btn-warning'>Edit</a></td>
-                                <td><a href='task.php?action=delete&id={$row['TaskID']}' class='btn btn-danger' onclick='return confirm(\"Are you sure?\")'>Delete</a></td>
+                                <td>{$row['TaskID']}</td>
+                                <td>{$row['Timestamp']}</td>
+                                <td>{$row['LogType']}</td>
                               </tr>";
                         $count++;
                     }
                 } else {
-                    echo "<tr><td colspan='10'>No tasks found</td></tr>";
+                    echo "<tr><td colspan='5'>No activity logs found</td></tr>";
                 }
                 ?>
             </tbody>
