cac079f659e47ac8cb27666f14eef6c8e531b64f
Fmdika <135718649+Fmdika@users.noreply.github.com>
2023-12-17 01:50:03 +0700

    logout

--- bookmark.php
+++ bookmark.php
@@ -1,17 +1,38 @@
 <?php
-include('connection.php');
+// bookmark.php
+require_once("connection.php");
 
-// Query untuk mendapatkan task yang dibookmark oleh pengguna
-$userID = 1; // Gantilah dengan ID pengguna yang sesuai
-$sql = "SELECT T.* FROM deadlinemu.Task T
-        JOIN deadlinemu.Bookmark B ON T.TaskID = B.TaskID
-        WHERE B.UserID = $userID";
-$result = mysqli_query($connection, $sql);
+// Function to get bookmarks for a specific user
+function getBookmarks($userId, $connection){
+    $query = "SELECT BookmarkID, TaskID FROM Bookmark WHERE UserID = $userId";
+    $result = $connection->query($query);
 
-// Memeriksa apakah query berhasil dieksekusi
-if (!$result) {
-    die("Error fetching bookmarked tasks: " . mysqli_error($connection));
+    if ($result->num_rows > 0) {
+        return $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        return [];
+    }
 }
+
+// Fungsi untuk menghapus bookmark
+if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
+    $taskId = $_GET['id'];
+    $deleteSql = "DELETE FROM deadlinemu.Bookmark WHERE TaskID = $taskId";
+
+    if (mysqli_query($connection, $deleteSql)) {
+        header("Location: task.php");
+        exit();
+    } else {
+        die("Error deleting bookmark: " . mysqli_error($connection));
+    }
+}
+
+
+// Assuming you have a user ID, you can get bookmarks for that user
+$userId = 1; // Replace with the actual user ID (you should get it from your authentication system)
+
+$bookmarks = getBookmarks($userId, $connection);
+
 ?>
 
 <!DOCTYPE html>
@@ -37,49 +58,72 @@ if (!$result) {
         <a href="task_and_category.php">Task and Category</a>
         <a href="activity_log.php">Task Log</a>
         <a href="bookmark.php">Bookmark</a>
-        <a href="#">Logout</a>
+        <a href="logout.php">Logout</a>
     </div>
 
     <div class="container">
-        <h2 class="mt-5 mb-3">Bookmarked Tasks</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Due Date</th>
-                    <th>Priority</th>
-                    <th>Status</th>
-                    <th>User ID</th>
-                    <th>Category ID</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                // Menampilkan data tugas yang dibookmark
-                if (mysqli_num_rows($result) > 0) {
-                    $count = 1;
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<tr>
-                                <td>{$count}</td>
-                                <td>{$row['Title']}</td>
-                                <td>{$row['Description']}</td>
-                                <td>{$row['DueDate']}</td>
-                                <td>{$row['Priority']}</td>
-                                <td>{$row['Status']}</td>
-                                <td>{$row['UserID']}</td>
-                                <td>{$row['CategoryID']}</td>
-                              </tr>";
-                        $count++;
-                    }
-                } else {
-                    echo "<tr><td colspan='8'>No bookmarked tasks found</td></tr>";
+    <h2 class="mt-5 mb-3">Bookmarks</h2>
+    <table class="table">
+            <?php
+            // Assuming you have a successful query execution here to get $categoryResult
+            $categoryResult = mysqli_query($connection, "SELECT * FROM category");
+
+            if (!$categoryResult) {
+                die("Error fetching categories: " . mysqli_error($connection));
+            }
+
+            while ($categoryRow = mysqli_fetch_assoc($categoryResult)) {
+                $categoryID = $categoryRow['CategoryID'];
+                $categoryName = $categoryRow['CategoryName'];
+
+                echo "<h3>$categoryName</h3>";
+
+                // Query to get bookmarks by category
+                $bookmarkSql = "SELECT b.*, t.Title as TaskTitle, t.Description as TaskDescription, t.DueDate
+                        FROM deadlinemu.Bookmark b
+                        INNER JOIN deadlinemu.Task t ON b.TaskID = t.TaskID
+                        WHERE t.CategoryID = $categoryID";  // Change b.CategoryID to t.CategoryID
+                $bookmarkResult = mysqli_query($connection, $bookmarkSql);
+
+                if (!$bookmarkResult) {
+                    die("Error fetching bookmarks: " . mysqli_error($connection));
                 }
-                ?>
-            </tbody>
-        </table>
-    </div>
+
+                echo "<table class='table'>
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Due Date</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>";
+
+                // Display bookmarks in the table
+                $count = 1;
+                while ($bookmarkRow = mysqli_fetch_assoc($bookmarkResult)) {
+                    echo "<tr>
+                            <td>{$count}</td>
+                            <td>{$bookmarkRow['TaskTitle']}</td>
+                            <td>{$bookmarkRow['TaskDescription']}</td>
+                            <td>{$bookmarkRow['DueDate']}</td>
+                            <td><a href='bookmark.php?action=delete&id={$bookmarkRow['TaskID']}' class='btn btn-danger' onclick='return confirm(\"Are you sure?\")'>Delete</a></td>
+                          </tr>";
+                    $count++;
+                }
+
+                echo "</tbody></table>";
+            }
+
+            // Don't forget to free the result set
+            mysqli_free_result($categoryResult);
+            ?>
+        </tbody>
+    </table>
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
         integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
--- task.php
+++ task.php
@@ -1,6 +1,47 @@
 <?php
 include('connection.php');
 
+
+session_start();
+// Fungsi add bookmark
+// Check if the user is logged in
+if (isset($_SESSION['user_id'])) {
+    $userId = $_SESSION['user_id'];
+    
+    // Check if bookmark action is triggered
+    if (isset($_GET['action']) && $_GET['action'] == 'add' && isset($_GET['id'])) {
+        $taskId = $_GET['id'];
+
+        // Check if the task is not already bookmarked
+        $checkBookmarkSql = "SELECT * FROM deadlinemu.Bookmark WHERE UserID = $userId AND TaskID = $taskId";
+        $checkBookmarkResult = mysqli_query($connection, $checkBookmarkSql);
+
+        if (!$checkBookmarkResult) {
+            die("Error checking bookmark: " . mysqli_error($connection));
+        }
+
+        if (mysqli_num_rows($checkBookmarkResult) == 0) {
+            // Task is not bookmarked, add bookmark
+            $addBookmarkSql = "INSERT INTO deadlinemu.Bookmark (UserID, TaskID) VALUES ($userId, $taskId)";
+            
+            if (mysqli_query($connection, $addBookmarkSql)) {
+                header("Location: task.php?notification=added");
+                exit();
+            } else {
+                die("Error adding bookmark: " . mysqli_error($connection));
+            }
+        } else {
+            // Task is already bookmarked, send notification
+            header("Location: task.php?notification=exists");
+            exit();
+        }
+    }
+} 
+else {
+    header("Location: login.php");
+    exit();
+}
+
 // Fungsi untuk menghapus tugas
 if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id'])) {
     $taskId = $_GET['id'];
@@ -14,28 +55,6 @@ if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['id']))
     }
 }
 
-// Menangani tindakan bookmark
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Handle bookmark action
-    if (isset($_POST['action']) && $_POST['action'] == 'bookmark' && isset($_POST['taskID'])) {
-        $taskID = $_POST['taskID'];
-        $userID = $_POST['userID'];
-
-        // Cek apakah tugas sudah di-bookmark
-        $checkBookmarkSql = "SELECT * FROM deadlinemu.Bookmark WHERE UserID = $userID AND TaskID = $taskID";
-        $checkBookmarkResult = mysqli_query($connection, $checkBookmarkSql);
-
-        if (mysqli_num_rows($checkBookmarkResult) == 0) {
-            // Jika belum di-bookmark, masukkan ke dalam tabel Bookmark
-            $insertBookmarkSql = "INSERT INTO deadlinemu.Bookmark (UserID, TaskID) VALUES ($userID, $taskID)";
-            mysqli_query($connection, $insertBookmarkSql);
-        } else {
-            // Jika sudah di-bookmark, hapus dari tabel Bookmark
-            $deleteBookmarkSql = "DELETE FROM deadlinemu.Bookmark WHERE UserID = $userID AND TaskID = $taskID";
-            mysqli_query($connection, $deleteBookmarkSql);
-        }
-    }
-}
 
 // Query untuk mengambil data tugas
 $sql = "SELECT * FROM deadlinemu.Task";
@@ -53,7 +72,7 @@ if (!$result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DeadlineMU - Task</title>
+    <title>DeadlineMU - Category</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="stylecategory.css">
@@ -70,7 +89,7 @@ if (!$result) {
         <a href="task_and_category.php">Task and Category</a>
         <a href="activity_log.php">Task Log</a>
         <a href="bookmark.php">Bookmark</a>
-        <a href="#">Logout</a>
+        <a href="logout.php">Logout</a>
     </div>
 
     <div class="container">
@@ -88,8 +107,8 @@ if (!$result) {
                     <th>User ID</th>
                     <th>Category ID</th>
                     <th>Edit</th>
-                    <th>Delete</th>
                     <th>Bookmark</th>
+                    <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -98,58 +117,64 @@ if (!$result) {
                 if (mysqli_num_rows($result) > 0) {
                     $count = 1;
                     while ($row = mysqli_fetch_assoc($result)) {
-                        $taskID = $row['TaskID'];
-                        $title = $row['Title'];
-                        $description = $row['Description'];
-                        $dueDate = $row['DueDate'];
-                        $priority = $row['Priority'];
-                        $status = $row['Status'];
-                        $userID = $row['UserID'];
-                        $categoryID = $row['CategoryID'];
-
-                        // Cek apakah tugas di-bookmark
-                        $checkBookmarkSql = "SELECT * FROM deadlinemu.Bookmark WHERE UserID = $userID AND TaskID = $taskID";
-                        $checkBookmarkResult = mysqli_query($connection, $checkBookmarkSql);
-
-                        $isBookmarked = mysqli_num_rows($checkBookmarkResult) > 0;
-
                         echo "<tr>
                                 <td>{$count}</td>
-                                <td>{$title}</td>
-                                <td>{$description}</td>
-                                <td>{$dueDate}</td>
-                                <td>{$priority}</td>
-                                <td>{$status}</td>
-                                <td>{$userID}</td>
-                                <td>{$categoryID}</td>
-                                <td><a href='v_edittask.php?id={$taskID}' class='btn btn-warning'>Edit</a></td>
-                                <td><a href='task.php?action=delete&id={$taskID}' class='btn btn-danger' onclick='return confirm(\"Are you sure?\")'>Delete</a></td>
-                                <td>
-                                    <form method='post' action='task.php'>
-                                        <input type='hidden' name='taskID' value='$taskID'>
-                                        <input type='hidden' name='userID' value='$userID'>
-                                        <button type='submit' name='action' value='bookmark' class='btn " . ($isBookmarked ? "btn-success" : "btn-outline-success") . "'>
-                                            " . ($isBookmarked ? "Bookmarked" : "Bookmark") . "
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>";
+                                <td>{$row['Title']}</td>
+                                <td>{$row['Description']}</td>
+                                <td>{$row['DueDate']}</td>
+                                <td>{$row['Priority']}</td>
+                                <td>{$row['Status']}</td>
+                                <td>{$row['UserID']}</td>
+                                <td>{$row['CategoryID']}</td>
+                                <td><a href='v_edittask.php?id={$row['TaskID']}' class='btn btn-warning'>Edit</a></td>                            
+                                <td><a href='task.php?action=add&id={$row['TaskID']}' class='btn btn-primary'>Bookmark</a></td>                            
+                                <td><a href='task.php?action=delete&id={$row['TaskID']}' class='btn btn-danger' onclick='return confirm(\"Are you sure?\")'>Delete</a></td>
+                              </tr>";
                         $count++;
                     }
                 } else {
-                    echo "<tr><td colspan='11'>No tasks found</td></tr>";
+                    echo "<tr><td colspan='10'>No tasks found</td></tr>";
                 }
                 ?>
             </tbody>
         </table>
     </div>
-
+    <div id="notification" class="alert alert-warning text-center" style="display: none;">
+        <strong id="notificationMessage"></strong>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
         integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous">
+    </script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            // Check if there's a notification parameter in the URL
+            const urlParams = new URLSearchParams(window.location.search);
+            const notification = urlParams.get('notification');
+
+            if (notification === 'added') {
+                showNotification('Task bookmarked successfully!', 'alert-success');
+            } else if (notification === 'exists') {
+                showNotification('Task is already bookmarked.', 'alert-warning');
+            }
+        });
+
+        function showNotification(message, alertClass) {
+            const notificationElement = document.getElementById('notification');
+            notificationElement.textContent = message;
+            notificationElement.classList.add(alertClass);
+            notificationElement.style.display = 'block';
+            setTimeout(function () {
+                notificationElement.style.display = 'none';
+                notificationElement.classList.remove(alertClass);
+            }, 2000);
+        }
+    </script>
+
 </body>
 
 </html>
